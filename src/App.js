2ff7545baa62162d0947b641511d5de1d1589030
Barrett Jessop <barrettmj11@gmail.com>
2023-05-04 18:32:09 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
-// import { useQuery } from '@apollo/react-hooks';
-// import { gql } from 'apollo-boost'
-import React from 'react';
-import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost'
 
 
 export default function App() {
   return (
-    <div className = "container">
+    <div>
       <h2>Welcome to my The SVU Women's Basketball Inventory Management System</h2>
       <br></br>
+      <DisplayAthletes/>
     </div>
   );
 }
 
+const getAthletes = gql`
+  query Athletes {
+    athletes {
+      id
+      athleteFullName
+      number
+      year
+      birthday
+    }
+  }
+`;
 
-// const getAthletes = gql`
-// query Athletes {
-//   athletes {
-//     athleteFullName
-//     number
-//     year
-//     birthday
-//   }
-// }
-// `;
+function DisplayAthletes() {
+  const { loading, error, data } = useQuery(getAthletes);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
-// function DisplayAthletes() {
-//     const { loading, error, data } = useQuery(getAthletes);
-  
-//     if (loading) return <p>Loading...</p>;
-//     if (error) return <p>Error : {error.message}</p>;
-  
-//     return data.locations.map(({ athleteFullName, number, year, birthday }) => (
-//       <div key={id}>
-//         <h3>SVU Athletes</h3>
-//         <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-//         <br />
-//         <b>About this location:</b>
-//         <p>This is a description</p>
-//         <br />
-//       </div>
-//     ));
-//     return data.locations.map(({ id }) => (
-//       <div key={id}>
-//         <h3>{id}</h3>
-//       </div>
-//     ));
-//   }
+  return data.locations.map(({ athleteFullName, number, year, birthday }) => (
+    <div>
+      <h3>Our Athletes</h3>
+      <br />
+      <b>About this location:</b>
+      <p>This is a list of all the Women on the SVU Women's Basketball team.</p>
+      <br />
+    </div>
+  ));
+}
