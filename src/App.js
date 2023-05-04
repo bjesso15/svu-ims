ff0191e210465fabdc5989e81df515f33c418ca6
Barrett Jessop <barrettmj11@gmail.com>
2023-05-04 17:58:19 -0400

    working on pages and app

--- src/App.js
+++ src/App.js
@@ -1,62 +1,50 @@
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost'
+// import { useQuery } from '@apollo/react-hooks';
+// import { gql } from 'apollo-boost'
+import React from 'react';
+import './App.css';
+
 
 export default function App() {
   return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <br />
-      <DisplayAthletes />
+    <div className = "container">
+      <h2>Welcome to my The SVU Women's Basketball Inventory Management System</h2>
+      <br></br>
     </div>
   );
 }
 
-const getRecipes = gql`
-query Recipes {
-  recipes {
-    ingredients
-    instructions
-    description
-    mealType
-    title
-    slug
-    id
-  }
-}
-`
 
-const GET_ATHLETES = gql` {
-  query MyQuery {
-    athletes {
-      athleteFullName
-      birthday
-      id
-      number
-      year
-    }
-  }
-}`;
+// const getAthletes = gql`
+// query Athletes {
+//   athletes {
+//     athleteFullName
+//     number
+//     year
+//     birthday
+//   }
+// }
+// `;
 
 
-function DisplayAthletes() {
-    const { loading, error, data } = useQuery(getRecipes);
+// function DisplayAthletes() {
+//     const { loading, error, data } = useQuery(getAthletes);
   
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
+//     if (loading) return <p>Loading...</p>;
+//     if (error) return <p>Error : {error.message}</p>;
   
-    // return data.locations.map(({ id, name, description, photo }) => (
-    //   <div key={id}>
-    //     <h3>{name}</h3>
-    //     <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-    //     <br />
-    //     <b>About this location:</b>
-    //     <p>{description}</p>
-    //     <br />
-    //   </div>
-    // ));
-    return data.locations.map(({ id }) => (
-      <div key={id}>
-        <h3>{id}</h3>
-      </div>
-    ));
-  }
+//     return data.locations.map(({ athleteFullName, number, year, birthday }) => (
+//       <div key={id}>
+//         <h3>SVU Athletes</h3>
+//         <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+//         <br />
+//         <b>About this location:</b>
+//         <p>This is a description</p>
+//         <br />
+//       </div>
+//     ));
+//     return data.locations.map(({ id }) => (
+//       <div key={id}>
+//         <h3>{id}</h3>
+//       </div>
+//     ));
+//   }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 
 
 const httpLink = new createHttpLink({
-  uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clgx5fh9m0odv01t86s67atv5/master',
+  uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clfsz1dlc2clm01uegdfzd77m/master',
   headers: {
     authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODI1NDQzMjMsImF1ZCI6WyJodHRwczovL2FwaS11cy1lYXN0LTEtc2hhcmVkLXVzZWExLTAyLmh5Z3JhcGguY29tL3YyL2NsZ3g1Zmg5bTBvZHYwMXQ4NnM2N2F0djUvbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6ImM3OTA3ZWIyLTgzNjMtNDhlMS1iMGQyLTZiZjM5NGU1ZTM2ZSIsImp0aSI6ImNsZ3k3ZzU0NjFtb2QwMXRhZmN3dzdnaTMifQ.Zy3orzSTOC4ppN3SBJfmbTdcTzlVL_YJJoce8XIVfZjd3eP_G4rOF1zne2M82Gg6btpIFs8ZhkigE72nNMgSJO7tScOU2hXS2t9AZbjIvJK0r6KIqpIDbeGKURGUdt7jRr8VkcnVvo7127mUu3nekgt8qYPj3FiD2q1BYvcv2MB5yxXL9oq_pD0Vw9qrdYBwU8CIFJm3U373pQ8BUqVJiL_q9QsJIEY_C48MpTnNi6uuWChshSJ3VjplOCRrt6nT02KK2_pj4sI0EdQ131kjoaiqk22d59Vex4sHpNvo5ErovMWcJRjUwgUxbQECxZhH7Ltk_pGKlnro7X0biiiRyR72pPlSWN_cLoTKgcyIfQTtd18LMoDS8NLLuKshLQwC-n7WJ_HRbfddaFv8LZCDBp1tEXPrZV0BFSchh2GaPsEeDFBUosliVe5dEohbZe9hCmfLgVFeIEKSm-ZC14lpY6cQsAXfzT-2-m16Y8u5S9w2YO1r4JIFlF7xbmzQJ2z09fhOmXDGEPjJae6NlLjG_HHdjDtVT44ZtQjyHa9aSnJA3EDJId2yBiI5mxIN-19TrXRn_1ywR8PBVOEr7QzL410oect1lI6FMxu9x14DKlYAxpRtQQBAc3g5S8Uwe3EfixQ8oLzRtaayEdpYuOQnmFxDK1NtblOaLMFDY-vQSkk`,
   },
@@ -27,6 +27,23 @@ root.render(
     <App />
   </ApolloProvider>,
 );
+
+// function Athletes({ onDogSelected }) {
+//   const { loading, error, data } = useQuery(GET_DOGS);
+
+//   if (loading) return 'Loading...';
+//   if (error) return `Error! ${error.message}`;
+
+//   return (
+//     <select name='dog' onChange={onDogSelected}>
+//       {data.dogs.map((dog) => (
+//         <option key={dog.id} value={dog.breed}>
+//           {dog.breed}
+//         </option>
+//       ))}
+//     </select>
+//   );
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
